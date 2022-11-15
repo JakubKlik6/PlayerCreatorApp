3bcf20db8eb4fc7ddbc172d234430537205b66c7
Jakub <Jakub.klik6@gmail.com>
2022-11-15 16:33:20 +0100

    Dodaj pliki projektów.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using First_MVC_APP.Models;
+
+namespace First_MVC_APP.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel doggo = new DogViewModel()
+            {Name = "Majki", Age = 11 };
+            return View(doggo);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
